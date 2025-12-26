579eb09b5ad386906dcb9e58c01a48e6a1996df0
shoaib Akram <shoaibakram2003@gmail.com>
2025-12-26 18:27:26 +0500

    modularized index.js. Created a single function for random number generation and image querying. Made a second function to select and display winner.

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+function num_gen_and_image_change(x) 
+{
+  var randomNumber = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImage = "dice" + randomNumber + ".png";
+  var randomImageSource = "images/" + randomDiceImage;
+  var image = document.querySelectorAll("img")[x];
+  image.setAttribute("src", randomImageSource);
+  return randomNumber;
 }
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+
+
+
+var num1 = num_gen_and_image_change(0);
+var num2 = num_gen_and_image_change(1);
+
+
+
+function declare_winner()
+{
+  if (num1 > num2) 
+  {
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  }
+  else if (num2 > num1) 
+  {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else 
+  {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
 }
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
-}
+
+declare_winner();
+
+
+
